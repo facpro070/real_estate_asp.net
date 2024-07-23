339a1c183e080539fff7b3edf6e0595df695fd72
facpro070 <facundo.pro070@gmail.com>
2024-07-23 10:50:52 +0300

    feat: combine db contexts -> single db schema

--- RealEstate.Shared/Data/Context/CombinedAppContext.cs
+++ RealEstate.Shared/Data/Context/CombinedAppContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RealEstate.Shared.Models.Entities.Users;
 using RealEstate.Shared.Models.Entities.Clients;
 using RealEstate.Shared.Models.Entities.Contracts;
 using RealEstate.Shared.Models.Entities.Estates;
@@ -6,12 +7,16 @@ using RealEstate.Shared.Models.Entities.Listings;
 
 namespace RealEstate.Shared.Data.Context
 {
-
-    public class CombinedAppContext : DbContext //: IdentityDbContext<ApplicationUser>
+    public class CombinedDBContext : DbContext
     {
-        public CombinedAppContext(DbContextOptions<CombinedAppContext> options)
-            : base(options) { }
-        public CombinedAppContext() { }
+        public CombinedDBContext(DbContextOptions<CombinedDBContext> options) : base(options) { }
+
+        // Users
+        public virtual DbSet<UserEntity> UserEntities { get; set; }
+        public virtual DbSet<UserAttribute> UserAttributes { get; set; }
+        public virtual DbSet<UserGroupMembership> UserGroupMemberships { get; set; }
+        public virtual DbSet<UserRoleMapping> UserRoleMappings { get; set; }
+        public virtual DbSet<UsernameLoginFailure> UsernameLoginFailures { get; set; }
 
         // Clients
         public DbSet<Client> Clients { get; set; }
@@ -19,14 +24,14 @@ namespace RealEstate.Shared.Data.Context
 
         // Contracts
         public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<Checklist> Checklists { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Project> Projects { get; set; }
         public DbSet<Contract_Invoice> Contract_Invoices { get; set; }
         public DbSet<Contract_Type> Contract_Type { get; set; }
         public DbSet<Payment_Frequency> Payment_Frequencies { get; set; }
         public DbSet<Under_Contract> Under_Contracts { get; set; }
-        public DbSet<Note> Notes { get; set; }
-        public DbSet<Offer> Offers { get; set; }
-        public DbSet<Project> Projects { get; set; }
-        public DbSet<Checklist> Checklists { get; set; }
 
         // Estates
         public DbSet<Estate> Estates { get; set; }
@@ -47,16 +52,46 @@ namespace RealEstate.Shared.Data.Context
         public DbSet<PriceHistory> PriceHistories { get; set; }
         public DbSet<Review> Review { get; set; }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Server=localhost;Port=5433;Database=realestate;User Id=realestateuser;Password=password;Integrated Security=true;Pooling=true;");
+            }
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.HasAnnotation("Relational:Collation", "en_US.utf8");
+
+            // Users
+            ConfigureUserEntities(modelBuilder);
+
+            // Clients
+            ConfigureClientEntities(modelBuilder);
+
+            // Contracts
+            ConfigureContractEntities(modelBuilder);
+
+            // Estates
+            ConfigureEstateEntities(modelBuilder);
+
+            // Listings
+            ConfigureListingEntities(modelBuilder);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        private void ConfigureUserEntities(ModelBuilder modelBuilder)
+        {
+            // User entity configurations (from UsersDBContext)
+            // ... (include all the entity configurations for UserEntity, UserAttribute, etc.)
+        }
+
+        private void ConfigureClientEntities(ModelBuilder modelBuilder)
+        {
             modelBuilder.Entity<Contact>().HasIndex(c => c.Id).IsUnique();
             modelBuilder.Entity<Client>().HasIndex(c => c.Id).IsUnique();
-            modelBuilder.Entity<Contract>().HasIndex(c => c.Id).IsUnique();
-            modelBuilder.Entity<Contract_Invoice>().HasIndex(ci => ci.Id).IsUnique();
-            modelBuilder.Entity<Contract_Type>().HasIndex(ci => ci.Id).IsUnique();
-            modelBuilder.Entity<Payment_Frequency>().HasIndex(ci => ci.Id).IsUnique();
-            modelBuilder.Entity<Under_Contract>().HasIndex(ci => ci.Id).IsUnique();
 
             modelBuilder
                 .Entity<Contact>()
@@ -66,11 +101,20 @@ namespace RealEstate.Shared.Data.Context
                 .OnDelete(DeleteBehavior.Restrict);
 
             modelBuilder
-                .Entity<Client>()
-                .HasOne(c => c.Contact)
-                .WithOne(cl => cl.Client)
-                .HasForeignKey<Contact>(cl => cl.Contact_Details)
-                .OnDelete(DeleteBehavior.Restrict);
+               .Entity<Client>()
+               .HasOne(c => c.Contact)
+               .WithOne(cl => cl.Client)
+               .HasForeignKey<Contact>(cl => cl.Contact_Details)
+               .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        private void ConfigureContractEntities(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contract>().HasIndex(c => c.Id).IsUnique();
+            modelBuilder.Entity<Contract_Invoice>().HasIndex(ci => ci.Id).IsUnique();
+            modelBuilder.Entity<Contract_Type>().HasIndex(ci => ci.Id).IsUnique();
+            modelBuilder.Entity<Payment_Frequency>().HasIndex(ci => ci.Id).IsUnique();
+            modelBuilder.Entity<Under_Contract>().HasIndex(ci => ci.Id).IsUnique();
 
             modelBuilder
                 .Entity<Contract>()
@@ -83,7 +127,10 @@ namespace RealEstate.Shared.Data.Context
                 .HasOne(cl => cl.Contact)
                 .WithOne(c => c.Client)
                 .HasForeignKey<Contact>(c => c.Client_Id);
+        }
 
+        private void ConfigureEstateEntities(ModelBuilder modelBuilder)
+        {
             modelBuilder
                 .Entity<Estate>()
                 .HasOne(cl => cl.City)
@@ -105,12 +152,15 @@ namespace RealEstate.Shared.Data.Context
                 .Ignore(ph => ph.OffersHistoryTouples);
 
             modelBuilder
-                .Entity<Listing>()
-                .HasOne(c => c.PriceHistory)
-                .WithOne(cl => cl.Listing)
-                .HasForeignKey<PriceHistory>(cl => cl.Listing_Id)
-                .OnDelete(DeleteBehavior.Restrict);
+               .Entity<Listing>()
+               .HasOne(c => c.PriceHistory)
+               .WithOne(cl => cl.Listing)
+               .HasForeignKey<PriceHistory>(cl => cl.Listing_Id)
+               .OnDelete(DeleteBehavior.Restrict);
+        }
 
+        private void ConfigureListingEntities(ModelBuilder modelBuilder)
+        {
             modelBuilder
                 .Entity<Employee>()
                 .HasOne(cl => cl.Company)
@@ -118,8 +168,12 @@ namespace RealEstate.Shared.Data.Context
                 .HasForeignKey(cl => cl.Id)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            base.OnModelCreating(modelBuilder);
+            modelBuilder
+               .Entity<Contact>()
+               .HasOne(co => co.Client)
+               .WithOne(cl => cl.Contact)
+               .HasForeignKey<Contact>(co => co.Client_Id)
+               .OnDelete(DeleteBehavior.Restrict);
         }
     }
-    // Comment this class out when creating migrations or updating db so nothing is generated twice
 }
